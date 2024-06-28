040c38d73162243f965c3cd45a69fb94c80fab8c
Florian Salbrechter <floriansalbrechter@gmail.com>
2024-06-28 19:26:35 +0800

    Add a queue

--- src/use-nano.ts
+++ src/use-nano.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Queue from 'queue';
 
 /**
  * 
@@ -37,3 +38,46 @@ export const useNano = (prompt: string) => {
 
     return output;
 };
+
+
+type StreamCallback = (chunk: string) => void;
+
+class PromptQueue {
+    private q = new Queue({ results: [], autostart: true, concurrency: 1 });
+    private session: any = null;
+
+    async init() {
+        const canCreate = await window.ai.canCreateTextSession();
+        if (canCreate === "readily" || canCreate === "after-download") {
+            this.session = await window.ai.createTextSession();
+        } else {
+            throw new Error("Unable to create text session");
+        }
+    }
+
+    async enqueue(
+        prompt: string,
+        streamCallback: StreamCallback
+    ) {
+        if (!this.session) {
+            await this.init();
+        }
+
+        let result = "";
+        this.q.push(async cb => {
+            const stream = this.session.promptStreaming(prompt);
+            for await (const chunk of stream as any) {
+                streamCallback(chunk);
+            }
+            if (cb) {
+                cb(undefined, result);
+            }
+        });
+    }
+
+    async clear() {
+        this.q.end();
+    }
+}
+
+export const promptQueue = new PromptQueue();
