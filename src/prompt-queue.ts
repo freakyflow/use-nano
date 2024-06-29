6f96f57338b3ae0671137733931c86a404773bd8
Morten Just <morten.just@gmail.com>
2024-06-29 08:05:59 +0200

    try to add cancelation

--- src/prompt-queue.ts
+++ src/prompt-queue.ts
@@ -5,10 +5,12 @@ type StreamCallback = (chunk: string) => void;
 class PromptQueue {
     private q = new Queue({ results: [], autostart: true, concurrency: 1 });
     private session: any = null;
+    private shouldCancel = false
 
     private static instance: PromptQueue;
     static getInstance() {
         if (!PromptQueue.instance) {
+            console.log("usenano: New queue")
             PromptQueue.instance = new PromptQueue();
         }
         return PromptQueue.instance;
@@ -23,6 +25,8 @@ class PromptQueue {
         }
     }
 
+
+
     async enqueue(
         prompt: string,
         streamCallback: StreamCallback
@@ -30,11 +34,20 @@ class PromptQueue {
         if (!this.session) {
             await this.init();
         }
-
         let result = "";
         this.q.push(async cb => {
+            
             const stream = this.session.promptStreaming(prompt);
             for await (const chunk of stream as any) {
+                if (this.shouldCancel) {
+                    console.log("usenano: canceled"); // i don't think this work. This is a good use case for a bunch of tests.
+                    if (cb) {
+                        cb(new Error("Operation canceled"), result);
+                    }
+                    this.shouldCancel = false;
+                    return;
+                }
+                console.log("usenano: streaming prompt: ", prompt.slice(0, 100), chunk.slice(0, 100))
                 streamCallback(chunk);
             }
             if (cb) {
@@ -44,6 +57,9 @@ class PromptQueue {
     }
 
     async clear() {
+        console.log("usenano: Clear queue")
+        this.session.prompt(" ") // see if we can force a cancelation this way
+        this.shouldCancel = true; // also tell for loops to exit 
         this.q.end();
     }
 }
